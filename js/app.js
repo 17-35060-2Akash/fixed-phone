10ef85c3d9b2dd3d2b56947fc2fc7d30cae84c5d
Akash <na.akash715@gmail.com>
2023-01-29 02:32:23 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,39 +1,44 @@
-const loadPhones = async(searchText, dataLimit) =>{
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+const loadPhones = async (searchText, dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhones(data.data, dataLimit);
 }
 
-const displayPhones = (phones, dataLimit) =>{
+const displayPhones = (phones, dataLimit) => {
     const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.textContent = ``;
     // phonesContainer.textContent = '';
     // display 10 phones only 
     const showAll = document.getElementById('show-all');
-    if(dataLimit && phones.length > 10) {
-        phones = phones.slice(0, 10);
+    if (dataLimit && phones.length > dataLimit) {
+
+        phones = phones.slice(0, dataLimit);
+
         showAll.classList.remove('d-none');
     }
-    else{
-        showAll.classList.add('d-hidden');
+    else {
+        showAll.classList.add('d-none');
     }
-    
+
+
 
     // display no phones found
     const noPhone = document.getElementById('no-found-message');
-    if(phones.length === 0){
+    if (phones.length === 0) {
         noPhone.classList.remove('d-none');
     }
-    else{
+    else {
         noPhone.classList.add('d-none');
     }
     // display all phones
-    phones.forEach(phone =>{
-        const phoneDiv  = document.createElement('div');
+
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
-        phonesContainer.innerHTML = `
+        phoneDiv.innerHTML = `
         <div class="card p-4">
-            <img src="${phone.images}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -48,7 +53,7 @@ const displayPhones = (phones, dataLimit) =>{
     toggleSpinner(false);
 }
 
-const processSearch = (dataLimit) =>{
+const processSearch = (dataLimit) => {
     toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -56,42 +61,44 @@ const processSearch = (dataLimit) =>{
 }
 
 // handle search button click
-document.getElementById('#btn-search').addEventListener('click', function(){
+document.getElementById('btn-search').addEventListener('click', function () {
     // start loader
     processSearch(10);
-})
+});
 
 // search input field enter key handler
-document.getElementById('search-field').addEventListener('keypress', function (e) {
-    if (e.key === 'enter') {
+document.getElementById('search-field').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
         processSearch(10);
     }
 });
 
+
+
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
-    if(!isLoading){
-        loaderSection.classList.remove('d-none')
+    if (isLoading) {
+        loaderSection.classList.remove('d-none');
     }
-    else{
+    else {
         loaderSection.classList.add('d-none');
     }
 }
 
 
 // not the best way to load show All
-document.getElementById('btn-show-all').addEventListener('click', function(){
+document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 })
 
-const loadPhoneDetails = async id =>{
-    const url =`www.openapi.programming-hero.com/api/phone/${id}`;
+const loadPhoneDetails = async id => {
+    const url = `www.openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhoneDetails(data.data);
 }
 
-const displayPhoneDetails = phone =>{
+const displayPhoneDetails = phone => {
     console.log(phone);
     const modalTitle = document.getElementById('phoneDetailModalLabel');
     modalTitle.innerText = phone.name;
@@ -105,4 +112,4 @@ const displayPhoneDetails = phone =>{
     `
 }
 
-loadPhones('apple');
+loadPhones('apple', 10);
